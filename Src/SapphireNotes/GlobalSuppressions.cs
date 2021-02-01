d999017e2c425ac1ca3a3b66eb80d77a08cf545f
David Timovski <davidtimovski@protonmail.com>
2021-02-01 22:47:30 +0100

    Note selection hotkeys

---
+++ Src/SapphireNotes/GlobalSuppressions.cs
@@ -0,0 +1,22 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.MainWindowViewModel.CtrlW")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.MainWindowViewModel.CtrlE")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.Alert")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.NotesDirectory")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.MoveNotesCheckBoxVisible")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.MoveNotes")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.AutoSaveIntervalLabels")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.SelectedAutoSaveIntervalIndex")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.AvailableThemes")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.SelectedThemeIndex")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.AvailableFonts")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.SelectedFontIndex")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.AvailableFontSizes")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.SelectedFontSizeIndex")]
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~P:SapphireNotes.ViewModels.PreferencesViewModel.ApplyEnabled")]
