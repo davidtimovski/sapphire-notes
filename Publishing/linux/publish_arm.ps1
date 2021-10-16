15797f683198178ed46eb7cc1860df26d04120c5
David Timovski <davidtimovski@protonmail.com>
2021-10-16 17:03:42 +0200

    Add publishing files

---
+++ Publishing/linux/publish_arm.ps1
@@ -0,0 +1,10 @@
+$Version = "0.6.0"
+
+cd "C:\Users\david\source\repos\sapphire-notes\Publishing\linux"
+
+New-Item -Path . -Name "output\sapphire-notes_${Version}_armhf" -ItemType "directory"
+Copy-Item -Path "C:\Users\david\source\repos\sapphire-notes\Publishing\linux\template_arm\*" -Destination "C:\Users\david\source\repos\sapphire-notes\Publishing\linux\output\sapphire-notes_${Version}_armhf\" -Recurse
+
+Copy-Item "C:\Users\david\source\repos\sapphire-notes\Publishing\linux\template_arm\DEBIAN\control" -Destination "C:\Users\david\source\repos\sapphire-notes\Publishing\linux\output\sapphire-notes_${Version}_armhf\DEBIAN\control"
+
+Copy-Item "C:\Users\david\source\repos\sapphire-notes\Src\SapphireNotes\bin\Release\net5.0\publish\SapphireNotes" -Destination "C:\Users\david\source\repos\sapphire-notes\Publishing\linux\output\sapphire-notes_${Version}_armhf\usr\bin\sapphire-notes"
