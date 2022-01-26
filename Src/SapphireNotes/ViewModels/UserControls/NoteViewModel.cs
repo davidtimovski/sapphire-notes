01a19ed18f16eaf67b1c6b173169363ebda93056
David Timovski <davidtimovski@protonmail.com>
2022-01-26 22:32:43 +0100

    Increase note name limit to 60 chars and handle forbidden chars

--- Src/SapphireNotes/ViewModels/UserControls/NoteViewModel.cs
+++ Src/SapphireNotes/ViewModels/UserControls/NoteViewModel.cs
@@ -46,6 +46,11 @@ namespace SapphireNotes.ViewModels.UserControls
             set => this.RaiseAndSetIfChanged(ref name, value);
         }
 
+        public string ShortenedName
+        {
+            get => name.Length > 30 ? $"{name[..28]}.." : name;
+        }
+
         private string content;
         public string Content
         {
